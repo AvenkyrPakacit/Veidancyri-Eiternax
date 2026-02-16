3550a241cb4e3044e02fac372db3221487d2981d
AvenkyrPakacit <168223716+AvenkyrPakacit@users.noreply.github.com>
2026-02-16 02:08:40 -0600

    NPC fix & Dialog.
    Various things.

--- stats/effects/voderaihunger/voderaihunger.lua
+++ stats/effects/voderaihunger/voderaihunger.lua
@@ -4,8 +4,10 @@ function init()
   d = 1
 end
 function update(dt)
+if(status.isResource("food")) then
   effect.setStatModifierGroup(statModifierGroup, {
     {stat = "powerMultiplier", effectiveMultiplier = 1 + (e - status.resourcePercentage("food") * e)},
     {stat = "protection", effectiveMultiplier = 1 - (d - status.resourcePercentage("food") * d)}
   })
+end
 end
